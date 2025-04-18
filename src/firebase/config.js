fced8b3ad1f47c75511b5ee51324a22371352c01
Emperor Aung <johnsonherryson222@gmail.com>
2025-04-18 17:09:20 +0630

    refactor: move Firebase configuration to environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAEliuS5VJr3dDbhcLsz8OZqR6yf5okncs",
-    authDomain: "lifesync-cc645.firebaseapp.com",
-    projectId: "lifesync-cc645",
-    storageBucket: "lifesync-cc645.firebasestorage.com",
-    messagingSenderId: "772509862327",
-    appId: "1:772509862327:web:ff955af8d9f06bb87dc032"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
